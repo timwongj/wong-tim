57e41b5f896f098d22b1f1cefd1d85924d7b8a6b
timothywong24 <timothywong7@gmail.com>
2015-02-09 21:29:25 -0500

    9 Feb 2015 2129

--- js/app.js
+++ js/app.js
@@ -31,6 +31,7 @@ function updateTime()
 
 $(document).ready(function(){
 	var dt, timeElapsed, minutes, seconds, milliseconds, dtElapsed;
+	$("#scramble").text(generateScramble(20));
 	$(document).on('keydown', function (e)
 	{
 		if (e.keyCode === 32)
@@ -58,11 +59,47 @@ $(document).ready(function(){
 				
 				timeElapsed = minutes + ":" + seconds + "." + milliseconds;
 				$("#timer").text(timeElapsed);
+				$("#scramble").text(generateScramble(20));
 				$("#times").append("<p id=\"times\">" + timeElapsed + "</p>");
 			}
 		}
 	});
 });
+
+function generateScramble(length)
+{
+	var previousMove = -1;
+    var secondPreviousMove = -1;
+    var scramble = "";
+    for (i = 0; i < length; i++)
+    {
+        var move = Math.floor((Math.random() * 6));
+        var direction = Math.floor((Math.random() * 3));
+        if (((previousMove == 0) && (secondPreviousMove != 1)) || ((previousMove == 1) && (secondPreviousMove != 0)) || ((previousMove == 2) && (secondPreviousMove != 3)) || ((previousMove == 3) && (secondPreviousMove != 2)) || ((previousMove == 4) && (secondPreviousMove != 5)) || ((previousMove == 5) && (secondPreviousMove != 4)))
+            secondPreviousMove = -1;
+        while ((move == previousMove) || (move == secondPreviousMove))
+            move = Math.floor((Math.random() * 6));
+        switch (move)
+        {
+            case 0: scramble = scramble.concat("U"); break;
+            case 1: scramble = scramble.concat("D"); break;
+            case 2: scramble = scramble.concat("L"); break;
+            case 3: scramble = scramble.concat("R"); break;
+            case 4: scramble = scramble.concat("F"); break;
+            case 5: scramble = scramble.concat("B"); break;
+        }
+        switch (direction)
+        {
+            case 1: scramble = scramble.concat("'"); break;
+            case 2: scramble = scramble.concat("2"); break;
+        }
+        secondPreviousMove = previousMove;
+        previousMove = move;
+        if (i < length - 1)
+            scramble = scramble.concat(" ");
+    }
+    return scramble;
+}
 
 function pad2(n)
 {
