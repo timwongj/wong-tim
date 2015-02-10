63f514f523ceafce9d9786ff202c14fb1c9f3c8e
timothywong24 <timothywong7@gmail.com>
2015-02-10 04:21:34 -0500

    10 Feb 2015 0421

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var start, stop, isTiming = 0, updateTimer = 0, allowedToUpdate = 0, solveNumber = localStorage.length;
+var start, stop, isTiming = 0, updateTimer = 0, allowedToUpdate = 0, solveNumber = localStorage.length, scrambleType = 3, scrambleLength = 20;
 
 $(document).ready(function()
 {
@@ -6,6 +6,48 @@ $(document).ready(function()
 	$("#resetButton").click(function () {
 		localStorage.clear();
 		location.reload();
+	});
+	$("#scramble2x2").click(function () {
+		scrambleType = 2;
+		scrambleLength = 10;
+		$("#scrambleLength").val(scrambleLength);
+		$("#scramble").text(generate2x2Scramble(scrambleLength));
+		$("#scramble").css('font-size','18pt');
+	});
+	$("#scramble3x3").click(function () {
+		scrambleType = 3;
+		scrambleLength = 20;
+		$("#scrambleLength").val(scrambleLength);
+		$("#scramble").text(generate3x3Scramble(scrambleLength));
+		$("#scramble").css('font-size','18pt');
+	});
+	$("#scramble4x4").click(function () {
+		scrambleType = 4;
+		scrambleLength = 40;
+		$("#scrambleLength").val(scrambleLength);
+		$("#scramble").text(generate4x4Scramble(scrambleLength));
+		$("#scramble").css('font-size','18pt');
+	});
+	$("#scramble5x5").click(function () {
+		scrambleType = 5;
+		scrambleLength = 60;
+		$("#scrambleLength").val(scrambleLength);
+		$("#scramble").text(generate5x5Scramble(scrambleLength));
+		$("#scramble").css('font-size','18pt');
+	});
+	$("#scramble6x6").click(function () {
+		scrambleType = 6;
+		scrambleLength = 80;
+		$("#scrambleLength").val(scrambleLength);
+		$("#scramble").text(generate6x6Scramble(scrambleLength));
+		$("#scramble").css('font-size','16pt');
+	});
+	$("#scramble7x7").click(function () {
+		scrambleType = 7;
+		scrambleLength = 100;
+		$("#scrambleLength").val(scrambleLength);
+		$("#scramble").text(generate7x7Scramble(scrambleLength));
+		$("#scramble").css('font-size','14pt');
 	});
     for(var i = 0; i < localStorage.length; i++)
 	{
@@ -15,7 +57,7 @@ $(document).ready(function()
     		$("#times").prepend("<tr>\n<td>" + key + "</td>\n<td>" + value + "</td>\n<td>" + "</td>\n<td>" + "</tr>");
 	}
 	var dt, timeElapsed, minutes, seconds, milliseconds, dtElapsed;
-	$("#scramble").text(generateScramble(20));
+	$("#scramble").text(generate3x3Scramble(20));
 	$(document).on('keydown', function (e)
 	{
 		if (e.keyCode === 32)
@@ -50,7 +92,20 @@ $(document).ready(function()
     				if (key != 'length')
     					$("#times").prepend("<tr>\n<td>" + key + "</td>\n<td>" + value + "</td>\n<td>" + "</td>\n<td>" + "</tr>");
 				}
-				$("#scramble").text(generateScramble(20));
+				if (scrambleType == 2)
+					$("#scramble").text(generate2x2Scramble(scrambleLength));
+				if (scrambleType == 3)
+					$("#scramble").text(generate3x3Scramble(scrambleLength));
+				if (scrambleType == 4)
+					$("#scramble").text(generate4x4Scramble(scrambleLength));
+				if (scrambleType == 5)
+					$("#scramble").text(generate5x5Scramble(scrambleLength));
+				if (scrambleType == 6)
+					$("#scramble").text(generate6x6Scramble(scrambleLength));
+				if (scrambleType == 7)
+					$("#scramble").text(generate7x7Scramble(scrambleLength));
+				else
+					$("#scramble").text(generate3x3Scramble(scrambleLength));
 			}
 		}
 	});
@@ -92,7 +147,55 @@ function updateTime()
     setTimeout(updateTime, 10);
 }
 
-function generateScramble(length)
+function generate2x2Scramble(length)
+{
+    var scramble = "";
+    var previousOrientation = -1;
+    for (i = 0; i < length; i++)
+    {
+        var turningOrientation = Math.floor((Math.random() * 3));
+        var innerTurningLayer = Math.floor((Math.random() * 2));
+        var turningDirection = Math.floor((Math.random() * 3));
+        while (turningOrientation == previousOrientation)
+        {
+            turningOrientation = Math.floor((Math.random() * 3));
+            innerTurningLayer = Math.floor((Math.random() * 2));
+            turningDirection = Math.floor((Math.random() * 3));
+        }
+        if (turningOrientation == 0)
+        {
+            if (innerTurningLayer == 0)
+                scramble = scramble.concat("U");
+            else
+                scramble = scramble.concat("D");
+        }
+        else if (turningOrientation == 1)
+        {
+            if (innerTurningLayer == 0)
+                scramble = scramble.concat("F");
+            else
+                scramble = scramble.concat("B");
+        }
+        else if (turningOrientation == 2)
+        {
+            if (innerTurningLayer == 0)
+                scramble = scramble.concat("L");
+            else
+                scramble = scramble.concat("R");
+        }
+        switch (turningDirection)
+        {
+            case 1: scramble = scramble.concat("'"); break;
+            case 2: scramble = scramble.concat("2"); break;
+        }
+        previousOrientation = turningOrientation;
+        if (i < length - 1)
+            scramble = scramble.concat(" ");
+    }
+    return scramble;
+}
+
+function generate3x3Scramble(length)
 {
 	var previousMove = -1;
     var secondPreviousMove = -1;
@@ -121,6 +224,359 @@ function generateScramble(length)
         }
         secondPreviousMove = previousMove;
         previousMove = move;
+        if (i < length - 1)
+            scramble = scramble.concat(" ");
+    }
+    return scramble;
+}
+
+function generate4x4Scramble(length)
+{
+    var scramble = "";
+    var layersTurned = 0;
+    var previousOrientation = -1;
+    var temp = -1;
+    for (i = 0; i < length; i++)
+    {
+        var turningOrientation = Math.floor((Math.random() * 3));
+        var turningLayer = Math.floor((Math.random() * 3));
+        var innerTurningLayer = Math.floor((Math.random() * 2));
+        var turningDirection = Math.floor((Math.random() * 3));
+        if (turningOrientation != previousOrientation)
+            layersTurned = 0;
+        switch (turningLayer)
+        {
+            case 0: temp = 1; break;
+            case 1: temp = 2; break;
+            case 2: temp = 4; break;
+        }
+        while ((previousOrientation == turningOrientation) && ((temp & layersTurned) > 0))
+        {
+            turningOrientation = Math.floor((Math.random() * 3));
+            turningLayer = Math.floor((Math.random() * 3));
+            innerTurningLayer = Math.floor((Math.random() * 2));
+            turningDirection = Math.floor((Math.random() * 3));
+            if (turningOrientation != previousOrientation)
+                layersTurned = 0;
+        }
+        if (turningOrientation == 0)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("U");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("D");
+            else if (innerTurningLayer == 0)
+                scramble = scramble.concat("Uw");
+            else
+                scramble = scramble.concat("Dw");
+        }
+        else if (turningOrientation == 1)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("F");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("B");
+            else if (innerTurningLayer == 0)
+                scramble = scramble.concat("Fw");
+            else
+                scramble = scramble.concat("Bw");
+        }
+        else if (turningOrientation == 2)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("L");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("R");
+            else if (innerTurningLayer == 0)
+                scramble = scramble.concat("Lw");
+            else
+                scramble = scramble.concat("Rw");
+        }
+        switch (turningLayer)
+        {
+            case 0: layersTurned += 1; break;
+            case 1: layersTurned += 2; break;
+            case 2: layersTurned += 4; break;
+        }
+        switch (turningDirection)
+        {
+            case 1: scramble = scramble.concat("'"); break;
+            case 2: scramble = scramble.concat("2"); break;
+        }
+        previousOrientation = turningOrientation;
+        if (i < length - 1)
+            scramble = scramble.concat(" ");
+    }
+    return scramble;
+}
+
+function generate5x5Scramble(length)
+{
+    var scramble = "";
+	var layersTurned = 0;
+    var previousOrientation = -1;
+    var temp = -1;
+    for (i = 0; i < length; i++)
+    {
+        var turningOrientation = Math.floor((Math.random() * 3));
+        var turningLayer = Math.floor((Math.random() * 4));
+        var turningDirection = Math.floor((Math.random() * 3));
+        if (turningOrientation != previousOrientation)
+            layersTurned = 0;
+        switch (turningLayer)
+        {
+            case 0: temp = 1; break;
+            case 1: temp = 2; break;
+            case 2: temp = 4; break;
+            case 3: temp = 8; break;
+        }
+        while ((previousOrientation == turningOrientation) && ((temp & layersTurned) > 0))
+        {
+            turningOrientation = Math.floor((Math.random() * 3));
+            turningLayer = Math.floor((Math.random() * 4));
+            turningDirection = Math.floor((Math.random() * 3));
+            if (turningOrientation != previousOrientation)
+                layersTurned = 0;
+        }
+        if (turningOrientation == 0)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("U");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("Uw");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Dw");
+            else
+                scramble = scramble.concat("D");
+        }
+        else if (turningOrientation == 1)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("F");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("Fw");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Bw");
+            else
+                scramble = scramble.concat("B");
+        }
+        else if (turningOrientation == 2)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("L");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("Lw");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Rw");
+            else
+                scramble = scramble.concat("R");
+        }
+        switch (turningLayer)
+        {
+            case 0: layersTurned += 1; break;
+            case 1: layersTurned += 2; break;
+            case 2: layersTurned += 4; break;
+            case 3: layersTurned += 8; break;
+        }
+        switch (turningDirection)
+        {
+            case 1: scramble = scramble.concat("'"); break;
+            case 2: scramble = scramble.concat("2"); break;
+        }
+        previousOrientation = turningOrientation;
+        if (i < length - 1)
+            scramble = scramble.concat(" ");
+    }
+    return scramble;
+}
+
+function generate6x6Scramble(length)
+{
+    var scramble = "";
+    var layersTurned = 0;
+    var previousOrientation = -1;
+    var temp = -1;
+    for (i = 0; i < length; i++)
+    {
+        var turningOrientation = Math.floor((Math.random() * 3));
+        var turningLayer = Math.floor((Math.random() * 5));
+        var innerTurningLayer = Math.floor((Math.random() * 2));
+        var turningDirection = Math.floor((Math.random() * 3));
+        if (turningOrientation != previousOrientation)
+            layersTurned = 0;
+        switch (turningLayer)
+        {
+            case 0: temp = 1; break;
+            case 1: temp = 2; break;
+            case 2: temp = 4; break;
+            case 3: temp = 8; break;
+            case 4: temp = 16; break;
+        }
+        while ((previousOrientation == turningOrientation) && ((temp & layersTurned) > 0))
+        {
+            turningOrientation = Math.floor((Math.random() * 3));
+            turningLayer = Math.floor((Math.random() * 5));
+            innerTurningLayer = Math.floor((Math.random() * 2));
+            turningDirection = Math.floor((Math.random() * 3));
+            if (turningOrientation != previousOrientation)
+                layersTurned = 0;
+        }
+        if (turningOrientation == 0)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("U");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("D");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Uw");
+            else if (turningLayer == 3)
+                scramble = scramble.concat("Dw");
+            else if (innerTurningLayer == 0)
+                scramble = scramble.concat("3Uw");
+            else
+                scramble = scramble.concat("3Dw");
+        }
+        else if (turningOrientation == 1)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("F");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("B");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Fw");
+            else if (turningLayer == 3)
+                scramble = scramble.concat("Bw");
+            else if (innerTurningLayer == 0)
+                scramble = scramble.concat("3Fw");
+            else
+                scramble = scramble.concat("3Bw");
+        }
+        else if (turningOrientation == 2)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("L");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("R");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Lw");
+            else if (turningLayer == 3)
+                scramble = scramble.concat("Rw");
+            else if (innerTurningLayer == 0)
+                scramble = scramble.concat("3Lw");
+            else
+                scramble = scramble.concat("3Rw");
+        }
+        switch (turningLayer)
+        {
+            case 0: layersTurned += 1; break;
+            case 1: layersTurned += 2; break;
+            case 2: layersTurned += 4; break;
+            case 3: layersTurned += 8; break;
+            case 4: layersTurned += 16; break;
+        }
+        switch (turningDirection)
+        {
+            case 1: scramble = scramble.concat("'"); break;
+            case 2: scramble = scramble.concat("2"); break;
+        }
+        previousOrientation = turningOrientation;
+        if (i < length - 1)
+            scramble = scramble.concat(" ");
+    }
+    return scramble;
+}
+
+function generate7x7Scramble(length)
+{
+    var scramble = "";
+    var layersTurned = 0;
+    var previousOrientation = -1;
+    var temp = -1;
+    for (i = 0; i < length; i++)
+    {
+        var turningOrientation = Math.floor((Math.random() * 3));
+        var turningLayer = Math.floor((Math.random() * 6));
+        var turningDirection = Math.floor((Math.random() * 3));
+        if (turningOrientation != previousOrientation)
+            layersTurned = 0;
+        switch (turningLayer)
+        {
+            case 0: temp = 1; break;
+            case 1: temp = 2; break;
+            case 2: temp = 4; break;
+            case 3: temp = 8; break;
+            case 4: temp = 16; break;
+            case 5: temp = 32; break;
+        }
+        while ((previousOrientation == turningOrientation) && ((temp & layersTurned) > 0))
+        {
+            turningOrientation = Math.floor((Math.random() * 3));
+            turningLayer = Math.floor((Math.random() * 6));
+            turningDirection = Math.floor((Math.random() * 3));
+            if (turningOrientation != previousOrientation)
+                layersTurned = 0;
+        }
+        if (turningOrientation == 0)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("U");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("D");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Uw");
+            else if (turningLayer == 3)
+                scramble = scramble.concat("Dw");
+            else if (turningLayer == 4)
+                scramble = scramble.concat("3Uw");
+            else
+                scramble = scramble.concat("3Dw");
+            
+        }
+        else if (turningOrientation == 1)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("F");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("B");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Fw");
+            else if (turningLayer == 3)
+                scramble = scramble.concat("Bw");
+            else if (turningLayer == 4)
+                scramble = scramble.concat("3Fw");
+            else
+                scramble = scramble.concat("3Bw");
+        }
+        else if (turningOrientation == 2)
+        {
+            if (turningLayer == 0)
+                scramble = scramble.concat("L");
+            else if (turningLayer == 1)
+                scramble = scramble.concat("R");
+            else if (turningLayer == 2)
+                scramble = scramble.concat("Lw");
+            else if (turningLayer == 3)
+                scramble = scramble.concat("Rw");
+            else if (turningLayer == 4)
+                scramble = scramble.concat("3Lw");
+            else
+                scramble = scramble.concat("3Rw");
+        }
+        switch (turningLayer)
+        {
+            case 0: layersTurned += 1; break;
+            case 1: layersTurned += 2; break;
+            case 2: layersTurned += 4; break;
+            case 3: layersTurned += 8; break;
+            case 4: layersTurned += 16; break;
+            case 5: layersTurned += 32; break;
+        }
+        switch (turningDirection)
+        {
+            case 1: scramble = scramble.concat("'"); break;
+            case 2: scramble = scramble.concat("2"); break;
+        }
+        previousOrientation = turningOrientation;
         if (i < length - 1)
             scramble = scramble.concat(" ");
     }
