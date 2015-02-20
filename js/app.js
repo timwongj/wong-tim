158b1a5766c51f7498ac56a30aace6fbd7fa0c46
timothywong24 <timothywong7@gmail.com>
2015-02-19 21:31:23 -0500

    19 Feb 2015 2131

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-$(document).ready(function() {
-	
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+	$routeProvider.
+		when("/", {templateUrl: "home.html"}).
+		when("/cornerComms", {templateUrl: "bldAlgorithms/cornerComms.html"}).
+		when("/edgeComms", {templateUrl: "bldAlgorithms/edgeComms.html"}).
+		when("/OPAlgs", {templateUrl: "bldAlgorithms/OPAlgs.html"}).
+		when("/M2Algs", {templateUrl: "bldAlgorithms/M2Algs.html"}).
+		when("/otherAlgs", {templateUrl: "bldAlgorithms/otherAlgs.html"}).
+		when("/notation", {templateUrl: "bldAlgorithms/notation.html"}).
+		otherwise({redirectTo: "/home"});
 });
