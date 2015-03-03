71feabcb2a8215b0a39aecfebc9d1e6c28151a34
timothywong24 <timothywong7@gmail.com>
2015-03-03 15:09:39 -0500

    3 Mar 2015 1509

--- js/app.js
+++ js/app.js
@@ -8,13 +8,14 @@ app.config(function ($routeProvider) {
 		when("/OPAlgs", {templateUrl: "bldAlgorithms/OPAlgs.html"}).
 		when("/M2Algs", {templateUrl: "bldAlgorithms/M2Algs.html"}).
 		when("/otherAlgs", {templateUrl: "bldAlgorithms/otherAlgs.html"}).
+		when("/scheme", {templateUrl: "bldAlgorithms/scheme.html"}).
 		when("/notation", {templateUrl: "bldAlgorithms/notation.html"}).
 		otherwise({redirectTo: "/"});
 });
 
 app.run(function($rootScope, $location) {
 	$rootScope.$on("$routeChangeSuccess", function(event, next, current) {
-      update();
+    	update();
 	});
 });
 
@@ -26,7 +27,7 @@ function update()
    		for (i = 1; i < lines.length; i++)
    		{
         	var line = lines[i].split(",");
-        	$("#cornerCommsTableBody").append("<tr><td>" + line[1] + "</td><td>" + line[2] + "</td><td>" + line[3] + "</td><td>" + line[4] + "</td><td>" + line[5] + "</td><td>" + line[6] + "</td></tr>");
+        	$("#cornerCommsTableBody").append("<tr><td class=\"centerCol\">" + line[1] + "</td><td class=\"centerCol\">" + line[2] + "</td><td>" + line[3] + "</td><td class=\"centerCol\">" + line[4] + "</td><td>" + line[5] + "</td><td class=\"centerCol\">" + line[6] + "</td></tr>");
 		}
 	}, "text");
 	$.get("data/edgeComms.csv", function(data) {
@@ -35,25 +36,73 @@ function update()
    		for (i = 1; i < lines.length; i++)
    		{
         	var line = lines[i].split(",");
-        	$("#edgeCommsTableBody").append("<tr><td>" + line[1] + "</td><td>" + line[2] + "</td><td>" + line[3] + "</td><td>" + line[4] + "</td><td>" + line[5] + "</td><td>" + line[6] + "</td></tr>");
+        	$("#edgeCommsTableBody").append("<tr><td class=\"centerCol\">" + line[1] + "</td><td class=\"centerCol\">" + line[2] + "</td><td>" + line[3] + "</td><td class=\"centerCol\">" + line[4] + "</td><td>" + line[5] + "</td><td class=\"centerCol\">" + line[6] + "</td></tr>");
 		}
 	}, "text");
 	$.get("data/OPAlgs.csv", function(data) {
-   		$("#OPAlgsTableHead").html("<tr><th>Target</th><th>Algorithm</th><th># Moves</th><th>Notes</th></tr>");
+   		$("#OPAlgsTableHead").html("<tr><th>Target</th><th>Primary Algorithm</th><th># Moves</th><th>Notes</th><th>Alternative Algorithm</th><th># Moves</th><th>Notes</th></tr>");
    		var lines = data.split("\n");
    		for (i = 1; i < lines.length; i++)
    		{
         	var line = lines[i].split(",");
-        	$("#OPAlgsTableBody").append("<tr><td>" + line[0] + "</td><td>" + line[1] + "</td><td>" + line[2] + "</td><td>" + line[3] + "</td></tr>");
+        	$("#OPAlgsTableBody").append("<tr><td class=\"centerCol\">" + line[0] + "</td><td>" + line[1] + "</td><td class=\"centerCol\">" + line[2] + "</td><td class=\"centerCol\">" + line[3] + "</td><td>" + line[4] + "</td><td class=\"centerCol\">" + line[5] + "</td><td class=\"centerCol\">" + line[6] + "</td></tr>");
 		}
 	}, "text");
 	$.get("data/M2Algs.csv", function(data) {
-   		$("#M2AlgsTableHead").html("<tr><th>Target</th><th>Algorithm</th><th># Moves</th><th>Notes</th></tr>");
+   		$("#M2AlgsTableHead").html("<tr><th>Target</th><th>Primary Algorithm</th><th># Moves</th><th>Notes</th><th>Alternative Algorithm</th><th># Moves</th><th>Notes</th></tr>");
    		var lines = data.split("\n");
    		for (i = 1; i < lines.length; i++)
    		{
         	var line = lines[i].split(",");
-        	$("#M2AlgsTableBody").append("<tr><td>" + line[0] + "</td><td>" + line[1] + "</td><td>" + line[2] + "</td><td>" + line[3] + "</td></tr>");
+        	$("#M2AlgsTableBody").append("<tr><td class=\"centerCol\">" + line[0] + "</td><td>" + line[1] + "</td><td class=\"centerCol\">" + line[2] + "</td><td class=\"centerCol\">" + line[3] + "</td><td>" + line[4] + "</td><td class=\"centerCol\">" + line[5] + "</td><td class=\"centerCol\">" + line[6] + "</td></tr>");
 		}
 	}, "text");
+	$(document).on("click", "#resetButton", function() {
+		localStorage.setItem("edges", JSON.stringify({UR:"UR", RU:"RU", UF:"UF", FU:"FU", UL:"UL", LU:"LU", UB:"UB", BU:"BU", DR:"DR", RD:"RD", DL:"DL", LD:"LD", DB:"DB", BD:"BD", FL:"FL", LF:"LF", FR:"FR", RF:"RF", BL:"BL", LB:"LB", BR:"BR", RB:"RB"}));
+		localStorage.setItem("corners", JSON.stringify({UFR:"UFR", RUF:"RUF", FRU:"FRU", UFL:"UFL", FLU:"FLU", LUF:"LUF", UBR:"UBR", RUB:"RUB", BRU:"BRU", DFR:"DFR", FRD:"FRD", RDF:"RDF", DFL:"DFL", LDF:"LDF", FLD:"FLD", DBL:"DBL", BLD:"BLD", LDB:"LDB", DBR:"DBR", RDB:"RDB", BRD:"BRD"}));
+		var edges = JSON.parse(localStorage.getItem("edges"));
+		var corners = JSON.parse(localStorage.getItem("corners"));
+		$("#inputUR").val(edges.UR); $("#inputRU").val(edges.RU);
+		$("#inputUF").val(edges.UF); $("#inputFU").val(edges.FU);
+		$("#inputUL").val(edges.UL); $("#inputLU").val(edges.LU);
+		$("#inputUB").val(edges.UB); $("#inputBU").val(edges.BU);
+		$("#inputDR").val(edges.DR); $("#inputRD").val(edges.RD);
+		$("#inputDL").val(edges.DL); $("#inputLD").val(edges.LD);
+		$("#inputDB").val(edges.DB); $("#inputBD").val(edges.BD);
+		$("#inputFL").val(edges.FL); $("#inputLF").val(edges.LF);
+		$("#inputFR").val(edges.FR); $("#inputRF").val(edges.RF);
+		$("#inputBL").val(edges.BL); $("#inputLB").val(edges.LB);
+		$("#inputBR").val(edges.BR); $("#inputRB").val(edges.RB);
+		$("#inputUFR").val(corners.UFR); $("#inputRUF").val(corners.RUF); $("#inputFRU").val(corners.FRU);
+		$("#inputUFL").val(corners.UFL); $("#inputFLU").val(corners.FLU); $("#inputLUF").val(corners.LUF);
+		$("#inputUBR").val(corners.UBR); $("#inputRUB").val(corners.RUB); $("#inputBRU").val(corners.BRU);
+		$("#inputDFR").val(corners.DFR); $("#inputFRD").val(corners.FRD); $("#inputRDF").val(corners.RDF);
+		$("#inputDFL").val(corners.DFL); $("#inputLDF").val(corners.LDF); $("#inputFLD").val(corners.FLD);
+		$("#inputDBL").val(corners.DBL); $("#inputBLD").val(corners.BLD); $("#inputLDB").val(corners.LDB);
+		$("#inputDBR").val(corners.DBR); $("#inputRDB").val(corners.RDB); $("#inputBRD").val(corners.BRD);
+	});
+	$(document).on("click", "#updateButton", function() {
+		var edges = JSON.parse(localStorage.getItem("edges"));
+		var corners = JSON.parse(localStorage.getItem("corners"));
+		edges.UR = $("#inputUR").val(); edges.RU = $("#inputRU").val();
+		edges.UF = $("#inputUF").val(); edges.FU = $("#inputFU").val();
+		edges.UL = $("#inputUL").val(); edges.LU = $("#inputLU").val();
+		edges.UB = $("#inputUB").val(); edges.BU = $("#inputBU").val();
+		edges.DR = $("#inputDR").val(); edges.RD = $("#inputRD").val();
+		edges.DL = $("#inputDL").val(); edges.LD = $("#inputLD").val();
+		edges.DB = $("#inputDB").val(); edges.BD = $("#inputBD").val();
+		edges.FL = $("#inputFL").val(); edges.LF = $("#inputLF").val();
+		edges.FR = $("#inputFR").val(); edges.RF = $("#inputRF").val();
+		edges.BL = $("#inputBL").val(); edges.LB = $("#inputLB").val();
+		edges.BR = $("#inputBR").val(); edges.RB = $("#inputRB").val();
+		corners.UFR = $("#inputUFR").val(); corners.RUF = $("#inputRUF").val(); corners.FRU = $("#inputFRU").val();
+		corners.UFL = $("#inputUFL").val(); corners.FLU = $("#inputFLU").val(); corners.LUF = $("#inputLUF").val();
+		corners.UBR = $("#inputUBR").val(); corners.RUB = $("#inputRUB").val(); corners.BRU = $("#inputBRU").val();
+		corners.DFR = $("#inputDFR").val(); corners.FRD = $("#inputFRD").val(); corners.RDF = $("#inputRDF").val();
+		corners.DFL = $("#inputDFL").val(); corners.LDF = $("#inputLDF").val(); corners.FLD = $("#inputFLD").val();
+		corners.DBL = $("#inputDBL").val(); corners.BLD = $("#inputBLD").val(); corners.LDB = $("#inputLDB").val();
+		corners.DBR = $("#inputDBR").val(); corners.RDB = $("#inputRDB").val(); corners.BRD = $("#inputBRD").val();
+		localStorage.setItem("edges", JSON.stringify(edges));
+		localStorage.setItem("corners", JSON.stringify(corners));
+	});
 }
